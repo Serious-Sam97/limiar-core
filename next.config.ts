7ca5369a66adb524f19421c7068537ee895e8273
Serious-Sam97 <samzuckerberg.jar@gmail.com>
2026-06-26 14:17:56 -0300

    Origins setup

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['limiarcore.com', '*.limiarcore.com'],
 };
 
 export default nextConfig;
